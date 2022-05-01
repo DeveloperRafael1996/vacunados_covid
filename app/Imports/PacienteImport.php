d8333392fd829a14b56cab2230cf1a82f1b430b9
DeveloperRafael1996 <rafa291996@hotmail.com>
2022-05-01 17:58:36 -0500

    fix date import excel laravel

--- app/Imports/PacienteImport.php
+++ app/Imports/PacienteImport.php
@@ -3,8 +3,12 @@
 namespace App\Imports;
 
 use App\Models\Paciente;
+use App\Models\Sector;
+use App\Models\Grupo;
+use App\Models\Edad;
 use Maatwebsite\Excel\Concerns\{Importable, ToModel, WithHeadingRow};
 use App\Models\PacientDosis;
+use DB;
 
 class PacienteImport implements ToModel,WithHeadingRow
 {
@@ -35,7 +39,7 @@ class PacienteImport implements ToModel,WithHeadingRow
                         $pacient->paciente_id = $paciente->id;
                         $pacient->dosi_id = 1;
                         $pacient->fabricante = $row["fab_primera_dosis"];
-                        $pacient->fecha_vacunacion ="2022-12-21";
+                        $pacient->fecha_vacunacion =  \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['fecha_primera_dosis'])->format('Y-m-d');
                         $pacient->save();
                     }
                 }
@@ -50,7 +54,7 @@ class PacienteImport implements ToModel,WithHeadingRow
                         $pacient->paciente_id = $paciente->id;
                         $pacient->dosi_id = 2;
                         $pacient->fabricante = $row["fab_segunda_dosis"];
-                        $pacient->fecha_vacunacion ="2022-12-21";
+                        $pacient->fecha_vacunacion =  \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['fecha_segunda_dosis'])->format('Y-m-d');
                         $pacient->save();
                     }
 
@@ -68,7 +72,7 @@ class PacienteImport implements ToModel,WithHeadingRow
                         $pacient->paciente_id = $paciente->id;
                         $pacient->dosi_id = 3;
                         $pacient->fabricante = $row["fab_tercera_dosis"];
-                        $pacient->fecha_vacunacion ="2022-12-21";
+                        $pacient->fecha_vacunacion =  \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['fecha_tercera_dosis'])->format('Y-m-d');
                         $pacient->save();
                     }
                 }
@@ -77,19 +81,47 @@ class PacienteImport implements ToModel,WithHeadingRow
             }
             else
             {
+
+                //SECTOR
+                $sector = DB::table('sectors')->where('descripcion','=',$row["sector"])->first();
+
+                if(!$sector) {
+                    $sector = new Sector();
+                    $sector->descripcion = $row["sector"];
+                    $sector->save();
+                }
+
+                //GRUPO RIESGO
+                $grupo = DB::table('grupo_riesgos')->where('descripcion','=',$row["grupo"])->first();
+                
+                if(!$grupo) {
+                    $grupo = new Grupo();
+                    $grupo->descripcion = $row["grupo"];
+                    $grupo->save();
+                }
+
+                //EDADES MINSA
+                $edad = DB::table('edad_minsas')->where('descripcion','=',$row["edades"])->first();
+                
+                if(!$edad) {
+                    $edad = new Edad();
+                    $edad->descripcion = $row["edades"];
+                    $edad->save();
+                }
+
                 $pacient = new Paciente;
                 $pacient->name_paciente =$row["name"];
                 $pacient->nro_documento =$row["nro_documento"];
-                $pacient->sector_id =$row["sector_id"];
-                $pacient->grupo_riesgo_id =$row["grupo_riesgo_id"];
-                $pacient->edad_minsa_id =$row["edad_minsa_id"];
-                $pacient->fecha_nacimiento ="2022-12-21";
+                $pacient->sector_id =$sector->id;
+                $pacient->grupo_riesgo_id =$grupo->id;
+                $pacient->edad_minsa_id = $edad->id;
+                $pacient->fecha_nacimiento=  \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['fecha_nacimiento'])->format('Y-m-d');
                 $pacient->genero =$row["genero"];
                 $pacient->ubigeo =$row["ubigeo"];
                 $pacient->departamento =$row["departamento"];
                 $pacient->provincia =$row["provincia"];
                 $pacient->distrito =$row["distrito"];
-                $pacient->fecha_vacunacion ="2022-12-21";
+                $pacient->fecha_vacunacion ="2022-12-12";
 
                 $pacient->save();
                 return $pacient;
